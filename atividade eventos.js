ced1ce30b212881f7b3af9e6a0d09c4e2cfe2a06
TIago <61856308+tiagopy2000@users.noreply.github.com>
2022-07-17 18:34:02 -0300

    Update atividade eventos.js

--- atividade eventos.js
+++ atividade eventos.js
@@ -1,26 +1,39 @@
-var idade = 0;
-var data =0;
-var liParticipantes= 0;
+let participantesli = [ "Luiz", "Ana", "Carlos" , "Tiago"]
+let idparticipantes = 0;
+let Npalestrante= "Tiago";
+
+let hoje = new Date();
+let diaEvento = prompt("Informe o dia do evento (dd)");
+let mesEvento = prompt ("Informe o mês do evento (mm)");
+let anoEvento = prompt( "Informe o ano do evento (aaaa)");
+let dataEvento = new Date(anoEvento, mesEvento - 1, diaEvento);
 
 
-idade = prompt ("Informe a sua idade");
+if (dataEvento <= hoje) {
+  console.log("Essa data não pode ser agendada");
 
+}else {
+  console.log("Data de Evento permitada");
 
-if (idade >=18 ) {
-   liParticipantes = prompt ("informe a quantida de pessoas que irão participar");
+}
 
-   if(liParticipantes <= 100) {
-      data = prompt ("informe a data do evento");
+if (participantesli.length <100 ){
+  console.log("Quantidade de participantes menor que 100, cadastro permitido");
+  idparticipantes = prompt("Informe a idade do participante");
+  if (idparticipantes >= 18){
+    console.log("Maior que 18 anos, pode continuar");
+    participantesli.push(prompt("Informe o seu nome"));
 
-      if (data.length >= 05/08/20){
-        console.log("cadastro realizado com sucesso");
-      }else{
-        console.log("cadastro não realizado data invalida");
-      }
-     
-   }else{
-    console.log("cadastro não realizado numero de participantes insuficiente");
-   }
+  }else{
+    console.log("Idade não permitida");
+  }
 }else{
-    console.log("cadastro não realizado, pessoas com menos de 18 não são permitidas");
+  console.log("Não é permitodo o cadastro a lista esta cheia");
+
+}
+
+console.log("Palestrante", Npalestrante);
+console.log("Lista de participantes ", participantesli.length, "participantes:")
+for (let i = 0; i < participantesli.length; i++){
+  console.log(participantesli[i]);
 }
